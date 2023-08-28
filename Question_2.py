7cabfb16788f48d9fa79a0a3d2829cfcf5ab1cfb
Deepak <53943221+Deepakmoon143@users.noreply.github.com>
2023-08-28 21:32:59 +0530

    Question_2.py

--- Question_2.py
+++ Question_2.py
@@ -1,3 +1,11 @@
-user = input("enter the string") 
-reverse = user[::-1]
-print("Reversed string:",reverse)
+def reverse_string(input_string):
+    reversed_str = ""
+    for i in input_string:
+        reversed_str = i + reversed_str
+    return reversed_str
+
+user_input = input("Enter a string: ")
+
+reversed_result = reverse_string(user_input)
+print(f"Original String:{user_input}")
+print(f"Original String:{reversed_result}")
